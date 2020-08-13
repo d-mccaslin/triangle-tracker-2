c0c1cc73b48cb3fe27e6357b180b4c148f4130b6
David McCaslin <davidmccaslin94@gmail.com>
2020-08-13 14:57:58 -0700

    fixed several bugs including syntax errors
    CO AUTHORED by: Jillian Gibson jillian.l.gibson@gmail.com

--- js/scripts.js
+++ js/scripts.js
@@ -8,47 +8,47 @@
 
 // Business Logic
 function equTriangle (length1, length2, length3) {
-  if (length1 === length2 && length1 === lenght3){
+  if (length1 === length2 && length1 === length3){
     return true;
   };
 };
 
 function isoTriangle (length1, length2, length3) {
-  if (length1 === length2 && length1 !== lenght3){
+  if (length1 === length2 && length1 !== length3){
     return true;
   };
 };
 
 function scalTriangle (length1, length2, length3) {
-  if (length1 !== length2 && length1 !== lenght3 && length2 !== length3){
+  if (length1 !== length2 && length1 !== length3 && length2 !== length3){
     return true;
   };
 };
 
 function notATriangle (length1, length2, length3) {
-  if (length1+length2 || length1+lenght3 <= length2|| length2+length3 <= length1){
+  if (length1+length2 || length1+length3 <= length2|| length2+length3 <= length1){
     return true;
   };
 };
 
 // User Interface Logic
 
-$true(document).ready(function () {
+$(document).ready(function () {
   $("#lengths").submit(function (event) {
     event.preventDefault ();
     const length1Input = parseInt($("#length1").val());
-    const length2Input = parseInt($("#length1").val());
-    const length3Input = parseInt($("#length1").val());
+    const length2Input = parseInt($("#length2").val());
+    const length3Input = parseInt($("#length3").val());
 
 
     if (equTriangle(length1Input, length2Input, length3Input)) {
-      $("#result").text("You made an equilateral triangle!")
-    if (isoTriangle(length1Input, length2Input, length3Input)) {
-        $("#result").text("You made an isosceles triangle!")
-    if (scalTriangle(length1Input, length2Input, length3Input)) {
-      $("#result").text("You made a scalene triangle!")
-    else {
-      $("#result").text("Whoops! You did not make a triangle.")
+      $("#resultText").text("You made an equilateral triangle!")
+    } else if (isoTriangle(length1Input, length2Input, length3Input)) {
+        $("#resultText").text("You made an isosceles triangle!")
+    } else if (scalTriangle(length1Input, length2Input, length3Input)) {
+      $("#resultText").text("You made a scalene triangle!")
+    } else {
+      $("#resultText").text("Whoops! You did not make a triangle.")
     }
   });
 });
